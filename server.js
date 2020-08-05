1ba916ed07f46f90cd091538fc8981b7d53cb7f3
MMMarz009 <mar0099887@gmail.com>
2020-08-05 18:28:25 -0500

    Setup Server

---
+++ server.js
@@ -0,0 +1,32 @@
+//consts
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const { urlencoded } = require('express');
+const app = express();
+const PORT= process.env.PORT || 3000;
+
+//require
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//apps
+app.use(logger('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'))
+
+
+//mongoose connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WorkoutTracker", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+
+//listen
+app.listen(PORT, () =>{
+    console.log(`App is running on port ${PORT}`);
+});
