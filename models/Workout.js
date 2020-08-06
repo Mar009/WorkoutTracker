f3a15bc2e6ed6aa44d56261026c40133fba4928e
MMMarz009 <mar0099887@gmail.com>
2020-08-05 21:40:53 -0500

    Set up Workout Logic

---
+++ models/Workout.js
@@ -0,0 +1,72 @@
+//const mongoose & Schema
+const { Schema } = require("mongoose");
+const Schema = mongoose.Schema;
+
+//const workoutSchema w/ properties
+const workoutSchema = new Schema(
+    {
+        //day
+        day: {
+            type: Date,
+            default: Date.NOW
+        },
+        //excercise
+        excercises: [
+            {
+                // type
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise type"
+                },
+                // name
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+                // duration
+                duration: {
+                    type: Number,
+                    require: "Enter an exercise duration in minutes"
+                },
+                // weight
+                weight: {
+                    type: Number
+                },
+                // reps
+                reps: {
+                    type: Number
+                },
+                // sets
+                sets: {
+                    type: Number
+                },
+                // distance
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    },
+    {
+       //virtual properties 
+       toJSON: {
+           virtuals: true
+       }
+    }
+);
+
+
+//anything that was dynamically created is added to the schema.
+workoutSchema.virtual("totalDuration").get(function(){
+    //return a reduced array of exercies to just the same of the durations
+    return this.excercises.reduce((total, exercise) => {
+        return total = exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+//exports
+module.exports = Workout;
